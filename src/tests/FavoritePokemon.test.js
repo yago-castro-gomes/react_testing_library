23bba1f79aca837e0f0e680bb5ef9e1c8d8a63a1
Yago Castro <yago.fanstour@gmail.com>
2022-11-22 15:36:59 -0300

    entrega requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemon } from '../pages';
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  test('Teste requisito 3', () => {
+    render(<FavoritePokemon />);
+    const favorites = screen.getByText(/no favorite pokémon found/i);
+
+    expect(favorites).toBeInTheDocument();
+  });
+});
