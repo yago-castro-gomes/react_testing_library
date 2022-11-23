60e6b4d13c4c093dcc59b1269437e6e1d15cbdd3
Yago Castro <yago.fanstour@gmail.com>
2022-11-23 17:40:12 -0300

    entrega requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,22 +1,38 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-describe('Teste o componente <Pokedex.js />', () => {
-  test('Teste requisito 5', () => {
-    const head = screen.getByRole('heading', {
-      name: /encountered pokémon/i,
-      level: 2,
-    });
-    const btnNext = screen.getByRole('button', {
-      name: /próximo pokémon/i,
-    });
-    expect(head).toBeInTheDocument();
-    expect(btnNext).toBeInTheDocument();
+describe('Teste o componente <Pokemon.js />', () => {
+  test('Teste requisito 6.1', async () => {
+    renderWithRouter(<App />);
+    const namePoke = await screen.getAllByTestId('pokemon-name');
+    const typePokemon = await screen.getAllByTestId('pokemon-type');
+    const pokemonWeight = await screen.getAllByTestId('pokemon-weight');
+    const imgPokemon = await screen.getAllByRole('img');
 
-    userEvent.click(btnNext);
-    
+    expect(namePoke[0]).toBeInTheDocument('Pikachu');
+    expect(typePokemon[0]).toHaveTextContent('Electric');
+    expect(pokemonWeight[0]).toBeInTheDocument(/average weight: 6\.0 kg/i);
+    expect(imgPokemon[0]).toHaveAttribute('alt', `${namePoke[0].innerHTML} sprite`);
+    expect(imgPokemon[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    // expect(imgPokemon[1]).toHaveAttribute('alt', `${namePoke[0].innerHTML} is marked as favorite`)
+  });
+  test('test requisito 6.2', async () => {
+    const { history } = renderWithRouter(<App />);
+    const linkDetails = await screen.getByRole('link', {
+      name: /more details/i,
+    });
+
+    userEvent.click(linkDetails);
+    expect(history.location.pathname).toBe(`/pokemon/${25}`);
+
+    const favoriteBtn = await screen.findAllByText(/pokémon favoritado\?/i);
+    userEvent.click(favoriteBtn[0]);
+    const imgFavorite = await screen.getAllByRole('img', {
+      name: /pikachu is marked as favorite/i,
+    });
+    expect(imgFavorite[0]).toBeInTheDocument();
+    expect(imgFavorite[0]).toHaveAttribute('src', '/star-icon.svg');
   });
 });
